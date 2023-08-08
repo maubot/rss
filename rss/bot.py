890e171e69c837cc6cdfc679123b4d9d37888731
A git user <public@lrose.de>
2023-08-08 06:43:48 +0200

    Revert "Search for video tags in RSS body"
    This reverts commit 1d3d0cc18f8d1e30103d4e9b9c1e30f82916c5d7.

--- rss/bot.py
+++ rss/bot.py
@@ -22,7 +22,6 @@ from time import mktime, time
 import asyncio
 import hashlib
 import html
-import re
 
 import aiohttp
 import attr
@@ -118,8 +117,6 @@ class RSSBot(Plugin):
         )
         msgtype = MessageType.NOTICE if sub.send_notice else MessageType.TEXT
         try:
-            m = re.search(r"<video.*src=[\"']([^\"']*)", entry.summary).group(1)
-            await self.client.send_markdown(sub.room_id, entry.summary + str(m))
             return await self.client.send_markdown(
                 sub.room_id, message, msgtype=msgtype, allow_html=True
             )
